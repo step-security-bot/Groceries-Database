a20c751125dcf620fe1122cc226aa7d1de248b4b
Michael-Alvear-Pro-Coder <117200965+Michael-Alvear-Pro-Coder@users.noreply.github.com>
2023-11-26 00:01:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,9 +6,14 @@ int main(){
 
     Database myDatabase("product_database.txt");
 
-    Product cake("food", "123", "cake", "A slice of cake.", "$4.99");
+    Product cake("food", "001", "cake", "a slice of cake", "$4.99");
+    Product cereal("food", "002", "cereal", "a box of cereal", "$4.00");
+    Product tv("technology", "003", "tv", "a television", "$500.00");
 
-    myDatabase.append(cake);
+    vector<Product> myProductVector = {cake, cereal, tv};
+
+    myDatabase.turnProductVectorIntoFile(myProductVector);
+
     
     
     return 0;
