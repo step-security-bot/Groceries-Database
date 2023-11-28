4d5ccb5eafac749176e0208dde005056333285a8
Michael-Alvear-Pro-Coder <117200965+Michael-Alvear-Pro-Coder@users.noreply.github.com>
2023-11-27 21:55:16 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,20 @@ int main(){
 
     Database myDatabase("product_database.txt");
 
-    Product cake("food", "001", "cake", "a slice of cake", "$4.99");
-    Product cereal("food", "002", "cereal", "a box of cereal", "$4.00");
-    Product tv("technology", "003", "tv", "a television", "$500.00");
+    Product appleSlices("food", "006", "apple slices", "sliced apples", "2.00");
+    Product cake("food", "001", "cake", "a slice of cake", "4.99");
+    Product cereal("food", "002", "cereal", "a box of cereal", "4.00");
+    Product tv("technology", "003", "tv", "a television", "500.00");
+    Product dogFood("pet supplies", "004", "dog food", "food for dogs", "20.00");
+    Product catFood("pet supplies", "005", "cat food", "food for cats", "25.00");
+    
 
-    vector<Product> myProductVector = {cake, cereal, tv};
+    vector<Product> myProductVector = {tv, cake, cereal, dogFood, appleSlices, catFood};
 
     myDatabase.turnProductVectorIntoFile(myProductVector);
 
-    
+    myDatabase.sortByType();
+
     
     return 0;
 
