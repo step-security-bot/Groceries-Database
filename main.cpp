f1aaafd5a6ae7fbac039df218aae09d0a573236f
Michael-Alvear-Pro-Coder <117200965+Michael-Alvear-Pro-Coder@users.noreply.github.com>
2023-12-01 14:27:16 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,96 @@
 #include "database.h"
-
+//#include <Windows.h>
 using namespace std;
 
-int main(){
+int main()
+{
 
+    bool temp = true;
+    int opt =1;
+    Product toFind;
+
+
+    cout << "Welcome Loading Database..." << endl << endl;
     Database myDatabase("product_database.txt");
 
-    Product appleSlices("food", "006", "apple slices", "sliced apples", "2.00");
-    Product cake("food", "001", "cake", "a slice of cake", "4.99");
-    Product cereal("food", "002", "cereal", "a box of cereal", "4.00");
-    Product tv("technology", "003", "tv", "a television", "500.00");
-    Product dogFood("pet supplies", "004", "dog food", "food for dogs", "20.00");
-    Product catFood("pet supplies", "005", "cat food", "food for cats", "25.00");
     
+   
+    do {
+        cout << "What would you like to do? " << endl;
+        cout << "(1) Add new product" << endl;
+        cout << "(2) Print Database " << endl;
+        cout << "(3) Item Number Look up " << endl;
+        cout << "(4) Edit product price" << endl;
+        cout << "(5) Sort by Price " << endl;
+        cout << "(6) Sort by type " << endl;
+        cout << "(7) Print out seperate file for given type " << endl;
+        cout << "(8) Delete product " << endl;
+        cout << "(9) Close " << endl;
 
-   vector<Product> myProductVector = {tv, cake, cereal, dogFood, appleSlices, catFood};
+        cout << "\n\nEnter your option: ";
+        cin >> opt;
+        cout << endl << endl;
 
-    myDatabase.turnProductVectorIntoFile(myProductVector);
+        switch (opt) {
+        case 1: // Add new product
+            
+            do
+            {
+                myDatabase.addNewProduct();
+                cout << "Enter 0 to stop, 1 to keep going: ";
+                cin >> temp;
+            } while (temp == true);
+            break;
+        case 2:
+            // Print Database
+            myDatabase.printAllProducts();
+            break;
+        case 3:
+            // searches for specifc item by unique number
+            cout << endl;
+            toFind = myDatabase.userSearchByItemNum();
+            toFind.print();
+            cout << endl; 
+            break;
+        case 4:
+            // Edit product price
+            myDatabase.printAllProducts();
+            myDatabase.editProductPrice();
+            break;
+        case 5:
+            // Sort by Price
+            myDatabase.sortByPrice();
+            break;
+        case 6:
+            // Sort by type
+            myDatabase.sortByType();
+            break;
+        case 7:
+            // groups all of same type toghter saves into file
+            myDatabase.filepertype();
 
-    Database myNewDatabase = myDatabase.makeNewFileOfProductsWithTheSameType("product_database.txt", "food");
+            break;
+        case 8:
+            // Delete product
+            //system("cls");
+            myDatabase.printAllProducts();
+            myDatabase.removeProduct();
+            break;
+        case 9:
+            // Close
+            cout << "Closing the program." << endl;
+            break;
+        case 32:
+            // Clears database (option hidden)
+            cout << "deleting database..." << endl;
+            myDatabase.clearDatabase();
+            break;
+        default:
+            cout << "Invalid choice. Please select a number from 1 to 9." << endl;
+        }
+        //system("pause");
+        //system("cls");
+    } while (opt != 9);
 
     return 0;
-
 }
